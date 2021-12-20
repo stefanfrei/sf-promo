09d32314052c104838a35ca95c017717df93f7bc
Stefan Frei <stefan.a.frei@gmail.com>
2021-12-20 18:01:24 +0100

    init

---
+++ resources/js/app.js
@@ -0,0 +1,14 @@
+require('./bootstrap')
+import Vue from 'vue'
+
+window.Vue = require('vue');
+
+// router
+import router from './routes.js';
+window.router = router;
+window.Fire = new Vue();
+
+const app = new Vue({
+    el: '#app',
+    router,
+}).$mount('#app');
